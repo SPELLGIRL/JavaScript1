2f640e68d7d1688438b417ff67062afbfc93d19f
SPELLGIRL <chepushilka@yandex.ru>
2019-03-17 16:59:26 +0300

    Попытка добавления localStorage с возможностью очистки корзины.

--- homework6/2/2.js
+++ homework6/2/2.js
@@ -58,14 +58,45 @@ const basket = {
      */
     add(goodName, goodPrice) {
         this.goods.push({name: goodName, price: goodPrice});
+        let obj = {name: goodName, price: goodPrice};
+        let serialObj = JSON.stringify(obj);
+        let myKey = localStorage.length.toString() + 'key';
+        localStorage.setItem(myKey, serialObj);
         this.render();
+    },
+
+    /**
+     * Загружает значения из localStorage.
+     */
+    loadGoods() {
+        let ls = localStorage;
+        for (let i = 0; i < ls.length; i++) {
+            let key = ls.key(i);
+            let returnObj = JSON.parse(ls.getItem(key));
+            this.goods.push(returnObj);
+        }
     },
 };
 
+
+if (localStorage) {
+    basket.loadGoods();
+}
+
 window.onload = () => basket.init();
+
 
 document.querySelectorAll('.buy-btn').forEach(el => {
     el.addEventListener('click', e => {
         basket.add(e.target.dataset.name, +e.target.dataset.price);
     })
+
+});
+document.querySelectorAll('.clear-btn').forEach(el => {
+    el.addEventListener('click', e => {
+        localStorage.clear();
+        basket.goods = [];
+        basket.render();
+    })
+
 });
